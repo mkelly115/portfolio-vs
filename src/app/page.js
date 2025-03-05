a95313f0fb5f73f0fbce620ef491b28b4a6eaad6
Michael <mikek222@gmail.com>
2025-03-05 14:30:47 -0500

    updated styling for landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,18 @@
-import "./styles.css"
+import "./styles.css";
 
 export default function Home() {
   return (
-    <div>
-      <img src="\placeholder.jpg" className="photo"/>
-    <div>
-          <h1>Michael Andrew Kelly Portfolio</h1>
+    <div className="container">
+      <img src="/placeholder.jpg" className="photo" alt="Placeholder"/>
+      <div className="content">
+        <h1>Michael Andrew Kelly Portfolio</h1>
+        <p className="bio">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Blandit vitae montes volutpat dolor ultrices cursus. 
+          Dui tortor non vestibulum feugiat; platea metus faucibus justo. Lacinia mi bibendum leo lobortis porta imperdiet 
+          parturient donec. Litora platea taciti netus per dapibus. Mattis velit inceptos dapibus tincidunt tristique feugiat mauris erat. 
+          Ultrices et mauris suscipit massa in senectus. Sociosqu maecenas integer odio dictumst hac taciti amet accumsan.
+        </p>
+      </div>
     </div>
-    </div>
-
   );
 }
