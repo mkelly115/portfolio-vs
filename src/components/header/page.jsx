fba504ac9e732fd38ddf0387fc466a5173bcef9d
Michael <mikek222@gmail.com>
2025-03-11 12:10:23 -0400

    update

--- src/components/header/page.jsx
+++ src/components/header/page.jsx
@@ -5,13 +5,17 @@ import "../header/styles.css"
 
 export default function Header() {
     return(
+
+        // Maybe I should redirect to home and have a seperate about me page?
+        // Need to look into it further for what could look good
         
         <nav className="header">
             <div className="name-card">
                 Michael Kelly
             </div>
             <div className="nav-links">
-            <Link href="/" className="header">About Me</Link>
+            <Link href="/" className="header">Home</Link>
+            <Link href="about" className="header">About </Link>
             <Link href="projects" className="header">Projects</Link>
             <Link href="resume" className="header">Resume</Link>
             <Link href="contact" className="header">Contact</Link>
